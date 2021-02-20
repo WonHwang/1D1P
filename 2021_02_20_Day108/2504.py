ee43220919d1b76c86a963750de9119479ddd454
황지원 <ediblepotato@gmail.com>
2021-02-20 10:25:10 +0900

    Day108 2021.02.20

---
+++ 2021_02_20_Day108/2504.py
@@ -0,0 +1,59 @@
+# 1D1P Day108 BOJ 2504번 괄호의 값 문제 - 2021.02.20
+
+string = input()
+
+stack = []
+
+for char in string:
+    if char == ')':
+        if stack and stack[-1] == '(':
+            stack.pop()
+    elif char == ']':
+        if stack and stack[-1] == '[':
+            stack.pop()
+    else:
+        stack.append(char)
+
+if stack:
+    is_valid = 0
+else:
+    is_valid = 1
+
+answer = 0
+if is_valid:
+    for char in string:
+        if char == '(' or char == '[':
+            stack.append(char)
+        
+        elif char == ')':
+            if stack[-1] == '(':
+                stack.pop()
+                stack.append(2)
+                continue
+            
+            tmp = 0
+            while type(stack[-1]) is int:
+                tmp += stack.pop()
+            
+            if stack[-1] == '(':
+                stack.pop()
+                stack.append(2 * tmp)
+        
+        elif char == ']':
+            if stack[-1] == '[':
+                stack.pop()
+                stack.append(3)
+                continue
+
+            tmp = 0
+            while type(stack[-1]) is int:
+                tmp += stack.pop()
+            
+            if stack[-1] == '[':
+                stack.pop()
+                stack.append(3 * tmp)
+    
+    while stack:
+        answer += stack.pop()
+
+print(answer)

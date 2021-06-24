69246e29095c2ca2dc7fa8276960a0eb7148f9d9
황지원 <ediblepotato@gmail.com>
2021-06-24 22:08:19 +0900

    Day232 2021.06.24

---
+++ 2021_06_24_Day232/16394.java
@@ -0,0 +1,9 @@
+// 1D1P Day232 BOJ 16394번 홍익대학교 문제 - 2021.06.24
+
+import java.util.*;
+class Main {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt()-1946);
+    }
+}

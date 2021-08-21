b88ba8245388ed37a23bcd011b711d09ce50bfbb
황지원 <ediblepotato@gmail.com>
2021-08-22 00:10:31 +0900

    Day290 2021.08.21

---
+++ 2021_08_21_Day290/9654.py
@@ -0,0 +1,3 @@
+# 1D1P Day290 BOJ 9654번 나무함대 문제 - 2021.08.22
+
+print('SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\nN2 Bomber      Heavy Fighter  Limited    21        \nJ-Type 327     Light Combat   Unlimited  1         \nNX Cruiser     Medium Fighter Limited    18        \nN1 Starfighter Medium Fighter Unlimited  25        \nRoyal Cruiser  Light Combat   Limited    4         ')

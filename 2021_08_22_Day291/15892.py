0d206ee3c2d08947bf39ab3b444ff55a5acfa848
황지원 <ediblepotato@gmail.com>
2021-08-22 19:28:03 +0900

    Day291 2021.08.22

--- 2021_08_22_Day291/15892.py
+++ 2021_08_22_Day291/15892.py
@@ -6,14 +6,14 @@ M = 1234567891
 L = int(input())
 string = input()
 
-square = [1] * 5
+square = [1] * 50
 
-for i in range(1, 5):
-    square[i] = square[i-1] * r
+for i in range(1, 50):
+    square[i] = (square[i-1] * r) % M
 
 answer = 0
 
 for i in range(L):
-    answer += (ord(string[i]) - ord('a') + 1) * square[i]
+    answer += ((ord(string[i]) - ord('a') + 1) * square[i]) % M
 
-print(answer)
+print(answer % M)
